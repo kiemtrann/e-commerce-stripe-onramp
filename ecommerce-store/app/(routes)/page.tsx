27d04eddb1334c90512dd9d72147dcec766ce8f9
Kiem Tran <kiem.tran@team.enouvo.com>
2023-07-25 17:08:52 +0700

    setup enviroment

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard('9f7b1fc5-ae90-4dec-aa4f-381b2b354d82');
 
   return (
     <Container>
